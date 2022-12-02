20085f12edfdbc206ab890eda5a5f3048ac3cb7d
Panasi <flashigi@gmail.com>
2022-12-02 12:45:53 +0100

    Controller Tests added

---
+++ src/test/java/com/panasi/interview_questions/controller/CategoryControllerTest.java
@@ -0,0 +1,186 @@
+package com.panasi.interview_questions.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.panasi.interview_questions.InterviewQuestionsApplication;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+		  webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		  classes = InterviewQuestionsApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+public class CategoryControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	                                         // Get
+	
+	@Test
+	public void showAllCategories_thenStatus200() throws Exception {
+
+	    mvc.perform(get("/categories")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("$[0].name", is("Java")))
+	      .andExpect(jsonPath("$[1].name", is("Java OOP")))
+	      .andExpect(jsonPath("$[1].parentId", is(1)))
+	      .andExpect(jsonPath("$[2].name", is("Java Collections")))
+	      .andExpect(jsonPath("$[2].parentId", is(1)));
+	}
+	
+	@Test
+	public void showSubcategories_thenStatus200() throws Exception {
+
+	    mvc.perform(get("/categories/1/subcategories")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("$[0].name", is("Java OOP")))
+	      .andExpect(jsonPath("$[0].parentId", is(1)))
+	      .andExpect(jsonPath("$[1].name", is("Java Collections")))
+	      .andExpect(jsonPath("$[1].parentId", is(1)));
+	}
+	
+	@Test
+	public void showCategoryById_thenStatus200() throws Exception {
+
+	    mvc.perform(get("/categories/1")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("name", is("Java")));
+
+	}
+	
+	@Test
+	public void showCategoryById_thenStatus404() throws Exception {
+
+	    mvc.perform(get("/categories/99")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isNotFound())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	  	  .andExpect(jsonPath("message", is("Element is not found")));
+
+	}
+	
+	@Test
+	public void showCategoryById_thenStatus400() throws Exception {
+
+	    mvc.perform(get("/categories/wtf")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isBadRequest())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	  	  .andExpect(jsonPath("message", is("The parameter 'id' of value 'wtf' could not be converted to type 'int'")));
+
+	}
+	
+											// Post
+	
+	@Test
+	public void addNewCategory_thenStatus201() throws Exception {
+
+	    mvc.perform(post("/categories")
+	      .contentType(MediaType.APPLICATION_JSON)
+	      .content("{\"name\": \"RandomCategory\"}"))
+	      .andExpect(status().isCreated());
+
+	}
+	
+											// Put
+	
+	@Test
+	public void updateCategory_thenStatus202() throws Exception {
+		
+	    mvc.perform(put("/categories/7")
+	      .contentType(MediaType.APPLICATION_JSON)
+	      .content("{\"name\": \"PHP Updated\"}"))
+	      .andExpect(status().isAccepted());
+	    
+	    mvc.perform(get("/categories/7")
+	  	  .contentType(MediaType.APPLICATION_JSON))
+	  	  .andExpect(status().isOk())
+	  	  .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	  	  .andExpect(jsonPath("name", is("PHP Updated")));
+
+	}
+	
+	@Test
+	public void updateCategory_thenStatus404() throws Exception {
+		
+	    mvc.perform(put("/categories/99")
+	      .contentType(MediaType.APPLICATION_JSON)
+	      .content("{\"name\": \"PHP Updated\"}"))
+	      .andExpect(status().isNotFound());
+
+	}
+	
+											// Delete
+	
+	@Test
+	public void deleteCategory_thenStatus200() throws Exception {
+		
+	    mvc.perform(delete("/categories/4")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk());
+	    
+	    mvc.perform(get("/categories/4")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isNotFound())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	  	  .andExpect(jsonPath("message", is("Element is not found")));
+	    
+	    mvc.perform(get("/categories/5")
+	  	  .contentType(MediaType.APPLICATION_JSON))
+	  	  .andExpect(status().isNotFound())
+	  	  .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	  	  .andExpect(jsonPath("message", is("Element is not found")));
+
+	}
+	
+	@Test
+	public void deleteCategory_thenStatus404() throws Exception {
+		
+	    mvc.perform(delete("/categories/99")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isNotFound())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("message", is("Element does not exist")));
+	      
+	    
+	}
+	
+	@Test
+	public void deleteCategory_thenStatus409() throws Exception {
+		
+	    mvc.perform(delete("/categories/6")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isConflict())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("message", is("Element is binded and can't be deleted")));
+	      
+	    
+	}
+	
+
+}
---
+++ src/test/java/com/panasi/interview_questions/controller/QuestionControllerTest.java
@@ -0,0 +1,174 @@
+package com.panasi.interview_questions.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.panasi.interview_questions.InterviewQuestionsApplication;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+		  webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		  classes = InterviewQuestionsApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+public class QuestionControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+												// Get
+	
+	@Test
+	public void showAllQuestions_thenStatus200() throws Exception {
+
+	    mvc.perform(get("/questions")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("$[0].question", is("What is Java?")))
+	      .andExpect(jsonPath("$[1].question", is("What is JVM?")))
+	      .andExpect(jsonPath("$[2].question", is("What is Inheritance?")));
+	}
+	
+	@Test
+	public void showQuestionsFromCategory_thenStatus200() throws Exception {
+
+	    mvc.perform(get("/questions/category/1")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("$[0].question", is("What is Java?")))
+	      .andExpect(jsonPath("$[1].question", is("What is JVM?")));
+	}
+	
+	@Test
+	public void showQuestionsFromSubcategories_thenStatus200() throws Exception {
+
+	    mvc.perform(get("/questions/subcategory/1")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("$[0].question", is("What is Java?")))
+	      .andExpect(jsonPath("$[1].question", is("What is JVM?")))
+	      .andExpect(jsonPath("$[2].question", is("What is Inheritance?")))
+	      .andExpect(jsonPath("$[3].question", is("What is ArrayList?")));
+	}
+	
+	@Test
+	public void showQuestion_thenStatus200() throws Exception {
+
+	    mvc.perform(get("/questions/1")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("question", is("What is Java?")));
+	}
+	
+	@Test
+	public void showQuestion_thenStatus404() throws Exception {
+
+	    mvc.perform(get("/questions/99")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isNotFound())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("message", is("Element is not found")));
+	}
+	
+	@Test
+	public void showQuestion_thenStatus400() throws Exception {
+
+	    mvc.perform(get("/questions/wtf")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isBadRequest())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("message", is("The parameter 'id' of value 'wtf' could not be converted to type 'int'")));
+	}
+	
+													// Post
+	
+	@Test
+	public void addNewQuestion_thenStatus201() throws Exception {
+
+		mvc.perform(post("/questions")
+			      .contentType(MediaType.APPLICATION_JSON)
+			      .content("{\"question\": \"RandomQuestion\","
+			      		+ "\"category\": {"
+			      		+ "\"id\": 6,"
+			      		+ "\"name\": \"Hibernate\"}}"))
+			      .andExpect(status().isCreated());
+	}
+	
+													// Put
+	
+	@Test
+	public void updateQuestion_thenStatus202() throws Exception {
+		
+	    mvc.perform(put("/questions/5")
+	      .contentType(MediaType.APPLICATION_JSON)
+	      .content("{\"answer\": \"Java Library\"}"))
+	      .andExpect(status().isAccepted());
+	    
+	    mvc.perform(get("/questions/5")
+	  	  .contentType(MediaType.APPLICATION_JSON))
+	  	  .andExpect(status().isOk())
+	  	  .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	  	  .andExpect(jsonPath("answer", is("Java Library")));
+
+	}
+	
+	@Test
+	public void updateQuestion_thenStatus404() throws Exception {
+		
+	    mvc.perform(put("/questions/99")
+	      .contentType(MediaType.APPLICATION_JSON)
+	      .content("{\"answer\": \"Java Library\"}"))
+	      .andExpect(status().isNotFound());
+
+	}
+	
+													// Delete
+	
+	@Test
+	public void deleteQuestion_thenStatus200() throws Exception {
+		
+	    mvc.perform(delete("/questions/6")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isOk());
+	    
+	    mvc.perform(get("/questions/6")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isNotFound())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	  	  .andExpect(jsonPath("message", is("Element is not found")));
+
+	}
+	
+	@Test
+	public void deleteQuestion_thenStatus404() throws Exception {
+		
+	    mvc.perform(delete("/questions/99")
+	      .contentType(MediaType.APPLICATION_JSON))
+	      .andExpect(status().isNotFound())
+	      .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+	      .andExpect(jsonPath("message", is("Element does not exist")));
+	      
+	    
+	}
+
+}
