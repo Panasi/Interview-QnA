a09eea2e36fc504852bdeed348c788fa34febd10
Panasi <flashigi@gmail.com>
2023-02-10 16:48:04 +0100

    implemented schedule email sending to inactive users

--- src/main/java/com/panasi/interview_questions/mail/service/EmailService.java
+++ src/main/java/com/panasi/interview_questions/mail/service/EmailService.java
@@ -1,17 +1,30 @@
 package com.panasi.interview_questions.mail.service;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.panasi.interview_questions.mail.entity.EmailDetails;
+import com.panasi.interview_questions.security.payload.ERole;
+import com.panasi.interview_questions.security.repository.UserRepository;
 
 @Service
 public class EmailService {
 	
 	@Autowired
 	private JavaMailSender javaMailSender;
+	
+	private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 	
 	public boolean sendEmail(EmailDetails emailDetails) {
 		
@@ -25,6 +38,7 @@ public class EmailService {
             return true;
         }
         catch (Exception e) {
+        	logger.error("Error while sending mail", e);
             return false;
         }
 		
@@ -42,8 +56,28 @@ public class EmailService {
             return true;
         }
         catch (Exception e) {
+        	logger.error("Error while sending mail", e);
             return false;
         }
+		
+	}
+	
+	@Scheduled(fixedDelay = 1000 * 60 * 24)
+	public void sendReminderEmail() {
+		
+		List<String> inactiveUserEmails = userRepository.findUserEmailsByRoleAndNotInQuestionList(ERole.ROLE_USER);
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setFrom("noreply@questionsandanswers.com");
+		mailMessage.setText("You haven't ask any questions yet. Try it now!");
+		mailMessage.setSubject("Reminder!");
+		inactiveUserEmails.forEach(email -> {
+			mailMessage.setTo(email);
+			try {
+				javaMailSender.send(mailMessage);
+			} catch (Exception e) {
+				logger.error("Error while sending mail", e);
+			}
+		});
 		
 	}
 
--- src/main/java/com/panasi/interview_questions/security/repository/UserRepository.java
+++ src/main/java/com/panasi/interview_questions/security/repository/UserRepository.java
@@ -1,15 +1,24 @@
 package com.panasi.interview_questions.security.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.panasi.interview_questions.security.payload.ERole;
 import com.panasi.interview_questions.security.repository.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 	
 	public User findByUsername(String username);
+	
 	public Boolean existsByUsername(String username);
+	
 	public Boolean existsByEmail(String email);
+	
+	@Query("SELECT u.email FROM User u INNER JOIN u.roles r WHERE r.name = ?1 AND u.id NOT IN (SELECT q.authorId FROM Question q)")
+	List<String> findUserEmailsByRoleAndNotInQuestionList(ERole role);
 
 }
